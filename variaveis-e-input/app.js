406afdda34a2608c2406ac5d92a86fbc41f5a0b8
Luiz R. Higashi <tofuzn@gmail.com>
2022-02-08 15:53:56 -0300

    Update app.js

--- variaveis-e-input/app.js
+++ variaveis-e-input/app.js
@@ -7,7 +7,7 @@ const minhaPrimeiraConstante = 'Minha primeira constante';
 console.log(minhaPrimeiraConstante);
 
 let leituraCampo;
-readLine.question('Por favor, digite alguma coisa: ', input => {
+readLine.question(`Por favor, digite alguma coisa: `, input => {
     leituraCampo = input;
     console.log(`O usuario digitou: ${leituraCampo}`);
     process.exit(0);
