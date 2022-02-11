fded121ccbd3f71195f9bef139b3d80e2c5ce617
tofuzn <85912233+tofuzn@users.noreply.github.com>
2022-02-10 22:03:07 -0300

    Update app.js

--- arrays-excecoes/app.js
+++ arrays-excecoes/app.js
@@ -11,15 +11,38 @@ const lenProdutosDisponiveis = produtosDisponiveis.length;
 const listaMercado = process.argv.slice(2);
 const lenListaMercado = listaMercado.length;
 
+var produtosEncontrados = [];
+var produtosNaoEncontrados = [];
+
 for (let i = 0; i < lenListaMercado; i++) {
+    let k = 0;
     for (let j = 0; j < lenProdutosDisponiveis; j++) {
         if (listaMercado[i] == produtosDisponiveis[j]) {
-            console.log(`encontrado: ${listaMercado[i]}`);
-        } else {
-            console.log(`nao encontrado: ${listaMercado[i]}`);
+            // console.log(`encontrado: ${listaMercado[i]}`);
+            k++;
         }
     }
+    if (k > 0) {
+        // console.log(`Produto encontrado: ${listaMercado[i]}`);
+        produtosEncontrados.push(listaMercado[i]);
+                
+    } else {
+        // console.log(`Produto nao encontrado: ${listaMercado[i]}`);s
+        produtosNaoEncontrados.push(listaMercado[i]);
+    }
+
 }
+
+produtosEncontrados.forEach(produto => {
+    console.log(`ENCONTRADO: ${produto}`);
+})
+
+produtosNaoEncontrados.forEach(produto => {
+    console.log(`NAO ENCONTRADO: ${produto}`);
+})
+
+produtosDisponiveis.sort();
+console.log(produtosDisponiveis);
 
 
 
